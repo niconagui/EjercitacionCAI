2da82d9fcd96f9be25922d787d614c5120e742b2
niconagui <57NA42470077@campus.economicas.uba.ar>
2024-04-03 22:37:49 -0300

    Este commit es la prueba para hacer parte un proyecto de una solucion

---
+++ GuiaEjerciciosCAI_1/EJ1.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Hola mundo, este es el ejercicio 1");
+int a = 5;
+Console.WriteLine(a);
---
+++ GuiaEjerciciosCAI_1/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Hola mundo, este es el ejercicio 1");
+int a = 4;
+Console.WriteLine(a);
